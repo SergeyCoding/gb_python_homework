f260c41bf79fc7ea006d09773c7346e03e6e3e8a
SergeyXP <s.pusthotin@gmail.com>
2022-10-12 21:49:50 +0400

    task0202.py

--- Seminar02/task0201.py
+++ Seminar02/task0201.py
@@ -1,2 +1,18 @@
 # Задача 1. Напишите программу, которая принимает на вход число N и выдает список факториалов для чисел от 1 до N.
 # N = 4 -> [ 1, 2, 6, 24 ] (1, 1*2, 1*2*3, 1*2*3*4)
+
+
+from math import factorial
+
+
+print("Семинар 2. Задача 1")
+
+number = int(input('Number: '))
+
+
+def Factorial():
+    pass
+
+
+for i in range(1, number+1):
+    print(Factorial())
--- Seminar02/task0202.py
+++ Seminar02/task0202.py
@@ -1,1 +1,9 @@
 # Задача 2. Выведите таблицу истинности для выражения ¬(X ∧ Y) ∨ Z.
+
+print("Семинар 2. Задача 1")
+
+print(f"X\tY\tZ\tnot(X and Y) or Z")
+for x in range(0, 2):
+    for y in range(0, 2):
+        for z in range(0, 2):
+            print(f"{x}\t{y}\t{z}\t{int(not(x and y) or z)}")
