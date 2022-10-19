4811e93616a32ae484305094fb927502ec08bca2
SergeyXP <s.pusthotin@gmail.com>
2022-10-19 19:55:56 +0400

    task0401

--- Seminar04/task0401.py
+++ Seminar04/task0401.py
@@ -1,2 +1,19 @@
 # Задача 1. Задайте натуральное число N. Напишите программу, которая составит список простых множителей числа N.
 # 60 -> 2, 2, 3, 5
+
+print("Семинар 4. Задача 1")
+
+number = int(input("Number: "))
+
+result = []
+
+k = 2
+while number > 1:
+    if number % k == 0:
+        result += [k]
+        number /= k
+    else:
+        k += 1
+
+
+print(result)
