3782af62164b1bf9c7f88a6c6a30433f32a38faf
SergeyXP <s.pusthotin@gmail.com>
2022-10-22 15:39:36 +0400

    task0503

--- Seminar05/task0504.py
+++ Seminar05/task0504.py
@@ -3,4 +3,19 @@
 
 print("Семинар 5. Задача 4")
 
-number = int(input("Number: "))
+# number = int(input("Number: "))
+
+
+def is_end_game():
+    pass
+
+
+def print_fld():
+    fld = [['.', '.', '.'], ['.', '.', '.'], ['.', '.', '.']]
+    for v in fld:
+        for h in v:
+            print(h, end='')
+        print()
+
+
+print_fld()
