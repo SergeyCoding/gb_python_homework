fd5474d70818cf3bd36e987fb7310c051dcf08da
SergeyXP <s.pusthotin@gmail.com>
2022-11-09 19:57:11 +0400

    task0601

--- Seminar06/task0603.py
+++ Seminar06/task0603.py
@@ -1,1 +1,12 @@
 # Задача 3. Найдите все простые несократимые дроби, лежащие между 0 и 1, знаменатель которых не превышает 11.
+
+import random
+
+
+print("Семинар 6. Задача 3")
+
+number = int(input("Количество чисел: "))
+
+lst = list(random.randint(1, 10) for _ in range(number))
+print(lst)
+print(list(filter(lambda x: x > 5, lst)))
