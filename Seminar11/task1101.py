ea0a1a4d38001b1f7fbf0b33b604ad34641ef6db
SergeyXP <s.pusthotin@gmail.com>
2022-11-30 09:21:09 +0400

    task1101

--- Seminar11/task1101.py
+++ Seminar11/task1101.py
@@ -1,6 +1,39 @@
-# Задача 1. Постройте график функции 𝑓(𝑥)=5𝑥^2+10𝑥−30
+# %% [markdown]
+# ## Задача 1.
+# Постройте график функции 𝑓(𝑥)=5𝑥^2+10𝑥−30
+#
 # По графику определите, при каких значения x значение функции отрицательно.
 
+# %%
+import matplotlib.pyplot as plt
+import numpy as np
 
-def aaa():
-    pass
+
+def fun(x):
+    return 5*x*x+10*x-30
+
+
+# %%
+x = np.arange(-10, 10.01, 0.01)
+plt.plot(x, fun(x))
+#plt.plot(x, 0*x)
+
+sp = plt.subplot(111)
+plt.xlabel(r'$x$')
+plt.ylabel(r'$f(x)$')
+plt.title(r'$f(x)=5*x^2+10*x-30$')
+plt.grid(True)
+sp.spines['top'].set_bounds()
+sp.spines['right'].set_bounds()
+sp.spines['left'].set_position('zero')
+sp.spines['bottom'].set_position('zero')
+
+# Подбор значений для определения границ отрицательных значений
+specify_bound = (-3.65, 1.65)
+
+plt.bar(specify_bound, height=100, width=0.1, bottom=-50, color='red')
+
+plt.show()
+
+# %%
+print(f"Отрицательные значения в диапазоне:{ specify_bound}")
