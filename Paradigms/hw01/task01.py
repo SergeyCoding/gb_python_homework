5daef1bf232e7cd7e82c506f45a69c03d093f89d
SergeyXP <s.pusthotin@gmail.com>
2023-11-06 11:47:24 +0400

    paradigms_hw_01

--- Paradigms/hw01/task01.py
+++ Paradigms/hw01/task01.py
@@ -2,18 +2,19 @@
 # Дан список целых чисел numbers. Необходимо написать в императивном стиле процедуру для
 # сортировки числа в списке в порядке убывания. Можно использовать любой алгоритм сортировки.
 
-arr=[2,3,4,5,2,3,1,23]
-
-print(arr)
-
 def sort_list_imprative(numbers):
     for i in range(len(numbers)):
-        for j in range(len(numbers)):
-            print(i,j)
-    pass
+        for j in range(i+1,len(numbers)):
+            if numbers[i]>numbers[j]:
+              k=numbers[i]
+              numbers[i]=numbers[j]
+              numbers[j]=k
     return numbers
 
+
+
+# Задача №2
+# Написать точно такую же процедуру, но в декларативном стиле
+
 def sort_list_declarative(numbers):
-    
-    pass
-    return numbers
+    return numbers.sort()
